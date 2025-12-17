fa6e7cf7d0c73099dd1549904338c0958a2c62ff
Jarsi B <jarsi2004@gmail.com>
2025-12-17 14:55:47 +0530

    committs

--- backend/server.js
+++ backend/server.js
@@ -182,7 +182,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:3004",
-  "frontend-dbte.vercel.app",
+  "frontend-ms19.vercel.app",
 ];
 
 app.use(
